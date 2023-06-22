70a2ca01db9c91fd9847ec0d5f52c807f168674b
Snehagupta1907 <snehagupta98930@gmail.com>
2023-06-23 00:45:31 +0530

    class prediction fixed

--- client.py
+++ client.py
@@ -1,20 +1,14 @@
 import requests
-
-# Specify the file path of the video you want to predict
 video_file_path = 'mixkit-married-couple-violently-arguing-4522-medium.mp4'
 
-# Create a POST request to the Flask server
 files = {'file': open(video_file_path, 'rb')}
 response = requests.post('http://127.0.0.1:5000/predict', files=files)
 
-# Parse the response JSON
 data = response.json()
 
-# Check if the request was successful
 if 'error' in data:
     print('Error:', data['error'])
 else:
-    # Get the predicted class and output video file path from the response
     predicted_class = data['predicted_class']
     output_video_path = data['output_video']
 
--- server.py
+++ server.py
@@ -6,75 +6,54 @@ from collections import deque
 
 app = Flask(__name__)
 
-# Configuration
 ALLOWED_EXTENSIONS = {'mp4', 'avi'}
 SEQUENCE_LENGTH = 16
 IMAGE_HEIGHT = 64
 IMAGE_WIDTH = 64
-CLASSES_LIST = ['NonViolence', 'Violence']  # Add your class labels here
+CLASSES_LIST = ['NonViolence', 'Violence'] 
 
-# Load the model
 MoBiLSTM_model = load_model('violence.h5')
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def predict_frames(video_file_path, output_file_path):
-    # Read from the video file.
+   
     video_reader = cv2.VideoCapture(video_file_path)
-
-    # Get the width and height of the video.
     original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
     original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    # VideoWriter to store the output video on disk.
     video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
                                    video_reader.get(cv2.CAP_PROP_FPS), (original_video_width, original_video_height))
 
-    # Declare a queue to store video frames.
     frames_queue = deque(maxlen=SEQUENCE_LENGTH)
 
-    # Store the predicted class in the video.
     predicted_class_name = ''
+    violence_count = 0
 
-    # Iterate until the video is accessed successfully.
     while video_reader.isOpened():
         ok, frame = video_reader.read()
 
         if not ok:
             break
-
-        # Resize the frame to fixed dimensions.
         resized_frame = cv2.resize(frame, (IMAGE_HEIGHT, IMAGE_WIDTH))
-
-        # Normalize the resized frame.
         normalized_frame = resized_frame / 255
-
-        # Append the pre-processed frame into the frames queue.
         frames_queue.append(normalized_frame)
-
-        # Check if the number of frames in the queue is equal to the fixed sequence length.
         if len(frames_queue) == SEQUENCE_LENGTH:
-            # Pass the normalized frames to the model and get the predicted probabilities.
             predicted_labels_probabilities = MoBiLSTM_model.predict(np.expand_dims(frames_queue, axis=0))[0]
-
-            # Get the index of the class with the highest probability.
             predicted_label = np.argmax(predicted_labels_probabilities)
-
-            # Get the class name using the retrieved index.
             predicted_class_name = CLASSES_LIST[predicted_label]
-
-        # Write the predicted class name on top of the frame.
+            if predicted_class_name == "Violence":
+                violence_count += 1
         if predicted_class_name == "Violence":
             cv2.putText(frame, predicted_class_name, (5, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 12)
         else:
             cv2.putText(frame, predicted_class_name, (5, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 12)
-
-        # Write the frame into the disk using the VideoWriter.
         video_writer.write(frame)
 
     video_reader.release()
     video_writer.release()
+    predicted_class_name = "Violence" if violence_count > 0 else "NonViolence"
 
     return predicted_class_name
 
@@ -91,7 +70,7 @@ def predict():
     if file and allowed_file(file.filename):
         file_path = 'video_file.mp4'
         file.save(file_path)
-        output_file_path = 'output1.mp4'
+        output_file_path = 'output.mp4'
         predicted_class = predict_frames(file_path, output_file_path)
         return jsonify({'message': 'Prediction completed', 'predicted_class': predicted_class, 'output_video': output_file_path})
 
